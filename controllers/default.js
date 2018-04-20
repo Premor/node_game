a112a6704c45c614c68ae39f31bc065dcaca65ae
Premor <spamerkik@gmail.com>
2018-04-20 22:27:52 +0300

    add ugle chat

--- controllers/default.js
+++ controllers/default.js
@@ -2,7 +2,8 @@ exports.install = function() {
 	
 	F.route('/',home)
 	F.route('/{path}/', view_page);
-	
+	F.websocket('/test/',test);
+	F.global.online_users = [];
 };
 
 
@@ -15,4 +16,30 @@ function home(){
 function view_page(path) {
 	// models/pages.js --> Controller.prototype.render()
 	this.view(path);
+}
+function test(){
+	this.on('open',(client)=>{
+		F.global.online_users.concat(client)
+		this.send(`Приветствуем ${client.id}`);
+		client.send('вы подключены')
+	})
+	this.on('close',(client)=>{
+		offline_user(client.id);
+		this.send(`${client.id} покинул нас`);
+	})
+	this.on('message',(client,message)=>{
+		this.send(`${message}`);
+	})
+	
+}
+
+function offline_user(id){
+	let buf=0;
+	for (i of F.global.online_users){
+		if (i.id == id){
+			break;
+		}
+		buf++;
+	}
+	F.global.online_users.splice(buf,1)
 }
